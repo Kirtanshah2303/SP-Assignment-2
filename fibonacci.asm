c16be29dedc14b1880ee02fc2dfd5bbfad0e3b15
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2023-07-24 18:07:15 +0530

    fibonacci updated

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,26 +1,27 @@
 section .data
-    result_msg db "The %dth Fibonacci number is: %d", 10, 0
-    n equ 10     
+    result_msg db 'The %dth Fibonacci number is: %d', 10, 0
+    n equ 10          ; Replace 10 with the desired value of n
 
 section .text
     global _start
 
 _start:
-    mov eax, 0
-    mov ebx, 1
-
+    mov eax, 0   ; fib[0] = 0
+    mov ebx, 1   ; fib[1] = 1
+    mov ecx, 2   ; Start the loop from index 2
 loop_start:
     add eax, ebx
     xchg eax, ebx
+    dec dword [n]   ; Decrement the value of n
+    jnz loop_start  ; Continue the loop if n is not zero
 
-    dec n
-    jnz loop_start  ; Jump to loop_start if n is not zero, otherwise, break the loop
-
-    push ebx
-    push n
-    push dword result_msg
+    mov r10, rbx
+    mov rdi, result_msg  
+    mov rsi, n           
+    mov rdx, r10        
     call printf
 
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    mov rbx, r10
+
+    xor edi, edi         ; System call number for exit
+    call exit
