9afb138d8043d537936f60b2ab49bad77f3d688a
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2023-07-24 18:29:08 +0530

    updated

--- fibonacci.asm
+++ fibonacci.asm
@@ -9,6 +9,7 @@ _start:
     mov eax, 0   ; fib[0] = 0
     mov ebx, 1   ; fib[1] = 1
     mov ecx, 2   ; Start the loop from index 2
+
 loop_start:
     add eax, ebx
     xchg eax, ebx
@@ -16,12 +17,13 @@ loop_start:
     jnz loop_start  ; Continue the loop if n is not zero
 
     ; Print the result directly without using printf
-    mov rsi, n
-    mov rdi, result_msg
-    mov rax, 0x1     ; System call number for write
+    mov rax, 0x4     ; System call number for sys_write
+    mov rdi, 0x1     ; File descriptor 1 (stdout)
+    mov rsi, result_msg
     mov rdx, 26      ; Length of the message (adjust if needed)
     syscall
 
     ; Exit the program
-    xor edi, edi     ; System call number for exit
-    call exit
+    xor edi, edi     ; System call number for sys_exit
+    mov eax, 0x60    ; Exit code 0
+    syscall
